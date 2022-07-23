a84499980c3b280126ace7361cfc22dd32fdba9d
EC2 Default User <ec2-user@ip-172-31-34-159.ec2.internal>
2022-07-23 11:58:25 +0000

    Initial commit

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build -t iyke497/ml_microservice .
 # Step 2: 
 # List docker images
-
+docker images iyke497/ml_microservice
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 iyke497/ml_microservice
--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=iyke497/ml_microservice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+minikube kubectl -- run ml-app --image=iyke497/ml_microservice --port=80 
 
 # Step 3:
 # List kubernetes pods
-
+minikube kubectl -- get pods
+my_pod=$(minikube kubectl -- get pods | grep ml-app)
 # Step 4:
 # Forward the container port to a host
-
+minikube kubectl -- port-forward pod/ml-app --address 0.0.0.0 8000:80
